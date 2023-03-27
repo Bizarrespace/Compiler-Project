ab7db5fa5270b72a37bd75e209c76ae2ee093523
Pascual Gonzales <34193469+GonzalesP@users.noreply.github.com>
2023-03-27 13:48:12 -0700

    Add files via upload
    1) bomba.txt - created an input file using UTF-8 with BOM format (which is what the professor uses for his text files). This is the default input file for our Assignment 2 program right now (if you wish to prompt the user for an input of a specific text file, edit the main.cpp file to ask for cin input instead of bomba.txt).
    2) lexer.cpp - adjusted a few things, including how the lexer keeps track of what line it's on.
    3) lexer.h - adjusted a few things, mentioned above
    4) main.cpp - added a function to account for text files that use BOM
    5) output.txt - output generated by bomba.txt
    6) syntax_analyzer.cpp - adjusted a few functions
    7) syntax_analyzer.h - adjusted a few things, including adding a new variable called err_line_number. This is kept separate from the lexer's line_number to keep track of where next token is expected (which might be different from where the lexer is currently at ie. via new lines)
    
    TODO: make error messages more specific (ie. saying that _ is expected isn't a very helpful explanation)
    TODO: might change approach of test cases. ie. use exception handling instead of a bunch of int returns

---
+++ syntax_analyzer.h
@@ -0,0 +1,87 @@
+#pragma once
+#ifndef SYNTAX_ANALYZER_H_
+#define SYNTAX_ANALYZER_H_
+
+/* ------------------------------- LIBRARIES ------------------------------- */
+#include <fstream>  // input file
+#include <string>
+#include <utility>  // pair
+#include <vector>  // message list
+
+#include "lexer.h"
+
+/* ------------------- DEFINE STATEMENTS FOR STRUCTURES ------------------- */
+typedef int flag;  // switch
+typedef std::string Message;
+typedef std::vector<Message> Rule_list;  // list of productions used for a token
+
+
+/* -------------------------------- CLASSES -------------------------------- */
+class Syntax_Analyzer {
+	private:
+		Lexer* lexer;  // get tokens from input file
+		Token current_token = { "", "" };  // keep track of most recent token
+		Rule_list Productions;  // record the productions used for each token
+		std::ofstream* ofs;  // print tokens + rules to output file
+		int err_line_number = 1;  // keep track of where next symbol is supposed
+		// to appear (and if it doesn't, print the error line number in ofs)
+
+		// syntax rules
+
+		void Opt_Function_Definitions();
+		int Function_Definitions_Start(int exit_on_error);
+		void Function_Definitions_Cont();
+		int Function(int exit_on_error);
+		void Opt_Parameter_List();
+		int Parameter_List_Start(int exit_on_error);
+		void Parameter_List_Cont();
+		int Parameter(int exit_on_error);
+		int Qualifier(int exit_on_error);
+		void Body();
+		void Opt_Declaration_List();
+		int Declaration_List_Start(int exit_on_error);
+		void Declaration_List_Cont();
+		int Declaration(int exit_on_error);
+		int IDs_Start(int exit_on_error);
+		void IDs_Cont();
+		int Statement_List_Start(int exit_on_error);
+		void Statement_List_Cont();
+		int Statement(int exit_on_error);
+		int Compound(int exit_on_error);
+		int Assign(int exit_on_error);
+		int If_Start(int exit_on_error);
+		int If_Cont(int exit_on_error);
+		int Return_Start(int exit_on_error);
+		int Return_Cont(int exit_on_error);
+		int Print(int exit_on_error);
+		int Scan(int exit_on_error);
+		int While(int exit_on_error);
+		int Condition(int exit_on_error);
+		int Relop(int exit_on_error);
+		int Expression_Start(int exit_on_error);
+		void Expression_Cont();
+		int Term_Start(int exit_on_error);
+		void Term_Cont();
+		int Factor(int exit_on_error);
+		int Primary_Start(int exit_on_error);
+		void Primary_Cont();
+
+		// helper functions
+
+		// terminal symbols (tokens)
+		int check_symbol(std::string symbol, int exit_on_error);
+		void copy_list(Rule_list original, Rule_list& copy);  // copy rule list
+		std::string convert_to_lowercase(std::string s);
+		int get_line_number();  // get position of lexer
+		void print_current_token();
+		void print_productions();
+		void print_error(Message err_msg);
+
+
+	public:
+		Syntax_Analyzer(std::ifstream* input_file_stream,
+			            std::ofstream* output_file_stream);  //  constructor
+		void Rat23S();  //  start syntax analysis
+};
+
+#endif
